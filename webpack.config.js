4d7e59b584ddba9c126ed0c5c9b58f68c328f2cf
Nerijus <dnerijus72@gmail.com>
2019-05-31 21:45:38 +0300

    Setup webpack and vue

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const { VueLoaderPlugin } = require('vue-loader');
+const HtmlPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+    ]
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    },
+    extensions: ['*', '.js', '.vue', '.json']
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlPlugin({ template: 'index.html' })
+  ]
+};
